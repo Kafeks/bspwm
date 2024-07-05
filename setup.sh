6fea4ba08a5dedc46a074476d38fd9c6ec612d60
Stephan Raabe <mail@stephan-raabe.de>
2024-07-05 08:56:36 +0200

    Updates

--- setup.sh
+++ setup.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
+# Check if package is installed
+_isInstalledPacman() {
+    package="$1";
+    check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")";
+    if [ -n "${check}" ] ; then
+        echo 0; #'0' means 'true' in Bash
+        return; #true
+    fi;
+    echo 1; #'1' means 'false' in Bash
+    return; #false
+}
+
+# Install required packages
+_installPackagesPacman() {
+    toInstall=();
+    for pkg; do
+        if [[ $(_isInstalledPacman "${pkg}") == 0 ]]; then
+            echo "${pkg} is already installed.";
+            continue;
+        fi;
+        toInstall+=("${pkg}");
+    done;
+    if [[ "${toInstall[@]}" == "" ]] ; then
+        # echo "All pacman packages are already installed.";
+        return;
+    fi;
+    printf "Package not installed:\n%s\n" "${toInstall[@]}";
+    sudo pacman --noconfirm -S "${toInstall[@]}";
+}
+
 clear
 
 # Some colors
@@ -50,6 +80,9 @@ if [ -d ~/Downloads/hyprland-starter ] ;then
     rm -rf ~/Downloads/hyprland-starter
     echo ":: Existing installation folder removed"
 fi
+
+# Check if git is installed
+_installPackagesPacman "git";
 
 # Clone the packages
 git clone --depth 1 https://gitlab.com/stephan-raabe/hyprland-starter.git
